726ffcaaefea184e8f36a50c5c348ca56d5fb189
CharlotteMajka <55383161+CharlotteMajka@users.noreply.github.com>
2020-02-25 21:25:40 +0100

    håber ikke jeg fucker noget op ved denne commit

--- src/ulitmatetictactoe/TictactoeMainController.java
+++ src/ulitmatetictactoe/TictactoeMainController.java
@@ -17,19 +17,7 @@ import javafx.scene.control.Label;
  * @author zilot
  */
 public class TictactoeMainController implements Initializable
-{
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event)
-    {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
-    @Override
+{  @Override
     public void initialize(URL url, ResourceBundle rb)
     {
         // TODO
